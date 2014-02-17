0e726378409c527e13f23bba7b80e1038e6e7f10
BorisJr <bjrmatos@gmail.com>
2014-02-16 22:48:40 -0500

    # Node Twitter demo
    * Put the api key and api secret strings from your twitter app in ./config.json
    * Run node app.js

---
+++ routes/auth-callback.js
@@ -0,0 +1,8 @@
+/*
+ * GET auth window.
+ */
+
+exports.index = function(req, res){
+  console.log('auth-callback,user', req.user);
+  res.render('auth-callback', { user: JSON.stringify(req.user) });
+};
